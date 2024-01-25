0348ca1671997e702c4b61d9f636dabb629750a9
gizo333 <124778443+gizo333@users.noreply.github.com>
2024-01-25 16:51:04 +0300

    список пользователей

--- mok/src/components/calendar-comp/calendar.jsx
+++ mok/src/components/calendar-comp/calendar.jsx
@@ -28,93 +28,89 @@ function Calendar({ isVisible, onDateClick }) {
     const totalDays = daysInMonth(currentDate);
     const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
     const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDay();
-  
+
     const prevMonthTotalDays = daysInMonth(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
-  
-    // Добавляем дни предыдущего месяца
-    for (let i = firstDayOfMonth - 1; i >= 0; i--) {
-      const day = prevMonthTotalDays - i;
-      days.push(
-        <div key={`prev-${day}`} className="other-month-day">
-          {day}
-        </div>
-      );
-    }
-  
+
+    // // Добавляем дни предыдущего месяца
+    // for (let i = firstDayOfMonth - 1; i >= 0; i--) {
+    //   const day = prevMonthTotalDays - i;
+    //   days.push(
+    //     <div key={`prev-${day}`} className="other-month-day" onClick={() => handleDayClick(day)}>
+    //       {day}
+    //     </div>
+    //   );
+    // }
+
     // Добавляем дни текущего месяца
     for (let i = 1; i <= totalDays; i++) {
       const date = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
       const isCurrentDay = currentDate.getDate() === i;
       const isSelectedDay = selectedDate && selectedDate.getDate() === i;
       const isInCurrentWeek = currentWeek === getWeekNumber(date);
-  
+
       let dayClassName = 'calendar-day';
       if (isCurrentDay) dayClassName += ' current-day';
       if (isSelectedDay) dayClassName += ' selected-day';
       if (isInCurrentWeek) dayClassName += ' current-week';
-  
-      // Добавляем класс для последних дней текущего месяца
-      if (i >= totalDays - lastDayOfMonth + 1) {
-        dayClassName += ' end-of-month';
-      }
-  
+
       days.push(
         <div key={`current-${i}`} className={dayClassName} onClick={() => handleDayClick(i)}>
           {i}
         </div>
       );
     }
-  
+
     // Добавляем дни следующего месяца
-    for (let i = 1; i <= 6 - lastDayOfMonth; i++) {
-      days.push(
-        <div key={`next-${i}`} className="other-month-day">
-          {i}
-        </div>
-      );
-    }
-  
+    // for (let i = 1; i <= 6 - lastDayOfMonth; i++) {
+    //   days.push(
+    //     <div key={`next-${i}`} className="other-month-day" onClick={() => handleDayClick(i)}>
+    //       {i}
+    //     </div>
+    //   );
+    // }
+
     return days;
   };
-  
-  
 
   const handleDayClick = (day) => {
     const newSelectedDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), day);
     setSelectedDate(newSelectedDate);
     const selectedWeek = getWeekNumber(newSelectedDate);
     setCurrentWeek(selectedWeek);
-    
+
     if (onDateClick) {
       onDateClick(newSelectedDate);
     }
   };
-  
 
-  const prevMonth = () => {
+  const handlePrevMonthClick = () => {
     setCurrentDate((prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1));
     setSelectedDate(null);
   };
 
-  const nextMonth = () => {
-    setCurrentDate((prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1));
+  const handleNextMonthClick = () => {
+    setCurrentDate((prevDate) => {
+      const nextMonth = prevDate.getMonth() + 1;
+      const year = nextMonth === 12 ? prevDate.getFullYear() + 1 : prevDate.getFullYear();
+      const month = nextMonth % 12;
+      return new Date(year, month, 1);
+    });
     setSelectedDate(null);
   };
+  
 
   return (
     <CSSTransition in={isVisible} timeout={300} classNames="calendar" unmountOnExit>
       <div className="calendar">
         <div className="calendar-header">
-          <button onClick={prevMonth}>&lt;</button>
-          <h2 className='calendar-data'>{currentDate.toLocaleString('en-US', { month: 'long', year: 'numeric' })}</h2>
-          <button onClick={nextMonth}>&gt;</button>
+          <button onClick={handlePrevMonthClick}>&lt;</button>
+          <h2 className="calendar-data">{currentDate.toLocaleString('en-US', { month: 'long', year: 'numeric' })}</h2>
+          <button onClick={handleNextMonthClick}>&gt;</button>
         </div>
-        <div className="calendar-grid">
-          {generateCalendar()}
-        </div>
+        <div className="calendar-grid">{generateCalendar()}</div>
       </div>
     </CSSTransition>
   );
-};
+}
 
 export default Calendar;
--- mok/src/components/group-menu/siteUsers.jsx
+++ mok/src/components/group-menu/siteUsers.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
-import { CSSTransition } from 'react-transition-group';
+import React, { useState } from 'react';
+import Table from 'rc-table';
 import '../../styles/siteUsers.css';
+import 'rc-table/assets/index.css';
+import { Input } from 'antd';
 
-
-
-  function SiteUsers({ isVisible }) {
-    return isVisible ? (
-      <CSSTransition in={isVisible} timeout={300} classNames="calendar" unmountOnExit>
-        <div className='usr-container'>
-          
-        </div>
-      </CSSTransition>
-    ) : null;
-  }
+function SiteUsers({ isVisible }) {
+  const [searchText, setSearchText] = useState('');
   
-  export default SiteUsers;
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Profession',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
+  const data = [
+    { key: 1, name: 'John Doe', age: 30, address: 'New York' },
+    { key: 2, name: 'Jane Smith', age: 25, address: 'Los Angeles' },
+    { key: 3, name: 'Bob Johnson', age: 40, address: 'Chicago' },
+  ];
+
+  const filteredData = data.filter(item =>
+    Object.values(item).some(value =>
+      value.toString().toLowerCase().includes(searchText.toLowerCase())
+    )
+  );
+
+  return isVisible ? (
+    <div className='usr-container'>
+      <Input className='search-table'
+        placeholder="Search..."
+        onChange={e => setSearchText(e.target.value)}
+        value={searchText}
+      />
+      <Table className='head-table' columns={columns} data={filteredData} />
+    </div>
+  ) : null;
+}
+
+export default SiteUsers;
